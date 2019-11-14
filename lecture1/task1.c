48ec2e3dc0d949a41f438d195a4878505e8da2d3
oleh <internetwork@ukr.net>
2019-11-14 10:17:09 +0200

    task swap64

--- lecture1/task1.c
+++ lecture1/task1.c
@@ -17,18 +17,40 @@ uint32_t swap32(uint32_t a)
 	res += (a&0xFF000000) >> 24;
 	return res;
 }
+uint64_t swap64(uint64_t a)
+{
+	uint64_t res;
+	res = (a&0x00000000000000FF) << 56;
+	res += (a&0x000000000000FF00) << 40;
+	res += (a&0x0000000000FF0000) << 24;
+	res += (a&0x00000000FF000000) << 8;
+	res += (a&0x000000FF00000000) >> 8;
+	res += (a&0x0000FF0000000000) >> 24;
+	res += (a&0x00FF000000000000) >> 40;
+	res += (a&0xFF00000000000000) >> 56;
+	return res;
+}
 
 int main(void)
 {
     uint16_t a = 100;
     uint32_t b = 1234567800;
-    uint32_t ret;
+    uint64_t r,c = 12345678901234567;
+    uint32_t ret,ret2;
 
     ret = swap16(a);
-    printf(" before: 0x%04x\n after: 0x%04x\n", a,ret);
+    printf(" before: 0x%04x\n after:  0x%04x\n", a,ret);
 
     ret = swap32(b);
-    printf(" before: 0x%04x\n after: 0x%04x\n", b,ret);
+    printf(" before: 0x%08x\n after:  0x%08x\n", b,ret);
+
+    r = swap64(c);
+    ret = c >> 32;
+    ret2 = c;
+    printf(" before: 0x%08x%08x\n", ret,ret2);
+    ret = r >> 32;
+    ret2 = r;
+    printf(" after:  0x%08x%08x\n", ret,ret2);
 
     return 0;
 }
