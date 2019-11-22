71cd6bddec6b9d53e7cb2ba52eb32d7da9d9db15
oleh <internetwork@ukr.net>
2019-11-22 15:10:31 +0200

    task3 stack Push()

--- lecture3/main.c
+++ lecture3/main.c
@@ -3,6 +3,7 @@
 int main(void)
 {
 	char istring[101], ostring[100];
+	int digit;
 
 	printf("   Exercise 1\n");
 	printf(" Input string (100 characters maximum!):\n");
@@ -10,6 +11,36 @@ int main(void)
 	printf(" Output string:\n");
 	copy_string(istring,ostring);
 	printf("%s\n",ostring);
+
+	printf("   Exercise 2\n");
+	printf(" Input 3 digits to push in stack:\n");
+	printf("input first digit: ");scanf("%d",&digit);
+	if(Push(digit) == 1)
+	{
+		printf("  First digit is in stack!\n");
+	}
+	else
+	{
+		printf("  Cannot push first digit in stack!\n");
+	}
+	printf("input second digit: ");scanf("%d",&digit);
+	if(Push(digit) == 1)
+	{
+		printf("  Second digit is in stack!\n");
+	}
+	else
+	{
+		printf("  Cannot push second digit in stack!\n");
+	}
+	printf("input third digit: ");scanf("%d",&digit);
+	if(Push(digit) == 1)
+	{
+		printf("  Third digit is in stack!\n");
+	}
+	else
+	{
+		printf("  Cannot push third digit in stack!\n");
+	}
 
 	return 0;
 }
--- lecture3/task3.c
+++ lecture3/task3.c
@@ -9,3 +9,17 @@ void copy_string(char *inputs,char *outputs)
 	uint8_t i=0;
 	while(outputs[i] = inputs[i])i++;
 }
+
+os_stack *my_stack = NULL;
+
+//allocates new element in memory (STACK)
+//if successful returns nonzero, otherwise return zero
+int Push(int new_element)
+{
+	os_stack *p_new_element = (os_stack *)malloc(sizeof(os_stack));
+	if(p_new_element == NULL)return 0;
+	p_new_element->number = new_element;
+	p_new_element->next = my_stack;
+	my_stack = p_new_element;
+	return 1;
+}
