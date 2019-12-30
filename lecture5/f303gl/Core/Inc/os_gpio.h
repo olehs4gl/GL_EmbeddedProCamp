8d4c08892f21af2e9997e6feae1286b243d3e324
oleh <internetwork@ukr.net>
2019-12-30 17:14:28 +0200

    my uart

---
+++ lecture5/f303gl/Core/Inc/os_gpio.h
@@ -0,0 +1,14 @@
+/*
+ * os_gpio.h
+ *
+ *  Created on: Dec 24, 2019
+ *      Author: oleh
+ */
+
+#ifndef INC_OS_GPIO_H_
+#define INC_OS_GPIO_H_
+
+void gpio_init(void);
+void gpio_blink(void);
+
+#endif /* INC_OS_GPIO_H_ */
---
+++ lecture5/readme.txt
@@ -0,0 +1,7 @@
+
+uart configuration:
+
+baud rate	115200
+parity		none
+
+uart available every 5 second periodicly, when lights on.
